1a9e900d732ac6789090591bcb14ba1262106a2e
maggielmyang <maggielmyang@gmail.com>
2025-08-19 15:07:37 -0400

    test

---
+++ src/new.rs
@@ -0,0 +1,11 @@
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)] // if we add new fields, give them default values when deserializing old state
+pub struct TemplateApp {
+    // Example stuff:
+    label: String,
+
+    // this how you opt-out of serialization of a member
+    #[serde(skip)]
+    value: f32,
+}
